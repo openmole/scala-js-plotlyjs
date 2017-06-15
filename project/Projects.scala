313e1ec3d1fc83d9d32bebce22542c26e3488c2c
Kyle Unverferth <kyle@kyleu.com>
2017-06-14 21:24:09 -0600

    Version 1.0.1, Scala 2.12 cross-build support.

--- project/Projects.scala
+++ project/Projects.scala
@@ -10,13 +10,13 @@ object Projects {
   val projectName = "scala-js-plotlyjs"
 
   object Versions {
-    val app = "1.0.0"
-    val scala = "2.11.8"
+    val app = "1.0.1"
+    val scalaVersions = Seq("2.11.8", "2.12.2")
   }
 
   private[this] val commonSettings = Seq(
     version := Versions.app,
-    scalaVersion := Versions.scala,
+    crossScalaVersions := Versions.scalaVersions,
     scalacOptions ++= Seq(
       "-encoding", "UTF-8", "-feature", "-deprecation", "-unchecked", "–Xcheck-null", "-Xfatal-warnings", "-Xlint",
       "-Ywarn-adapted-args", /* "-Ywarn-dead-code", */ "-Ywarn-inaccessible", "-Ywarn-nullary-override", "-Ywarn-numeric-widen"
@@ -45,7 +45,7 @@ object Projects {
     bintrayVcsUrl := Some("git:git@github.com:DefinitelyScala/scala-js-plotlyjs.git"),
     publishMavenStyle := true,
     licenses += ("MIT", url("http://opensource.org/licenses/MIT")),
-    libraryDependencies ++= Seq("org.scala-js" %%% "scalajs-dom" % "0.8.1"),
+    libraryDependencies ++= Seq("org.scala-js" %%% "scalajs-dom" % "0.9.2"),
     scalaJSStage in Global := FastOptStage
   )
 
