a1327b6edf9c0ce6155d12c70ce03aa0eee04783
Mathieu <mathieu.leclaire@iscpif.fr>
2017-10-09 18:49:02 +0200

    Add typed events

--- src/main/scala/com/definitelyscala/plotlyjs/PlotlyConts.scala
+++ src/main/scala/com/definitelyscala/plotlyjs/PlotlyConts.scala
@@ -3,12 +3,22 @@ package com.definitelyscala.plotlyjs
 package object plotlyConts {
 
   type PlotMode = String
+  type PlotEvent = String
 
   object PlotMode {
     val LINES: PlotMode = "lines"
     val MARKERS: PlotMode = "markers"
     val TEXT: PlotMode = "text"
     val NONE: PlotMode = "none"
+  }
+
+  object PlotEvent {
+    val HOVER: PlotEvent = "plotly_hover"
+    val UNHOVER: PlotEvent = "plotly_unhover"
+    val CLICK: PlotEvent = "plotly_click"
+    val DOUBLE_CLICK: PlotEvent = "plotly_doubleclick"
+    val SELECTED: PlotEvent = "plotly_selected"
+    val AFTER_PLOT: PlotEvent = "after_plot"
   }
 
 }
